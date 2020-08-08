cd4c10e85ce15793d56e3954add1b8ce9a97a5fb
anderson <andersongironza56@gmail.com>
2020-08-08 08:46:24 -0500

    primer proyecto

---
+++ Proyecto.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+
+
+raiz=Tk()
+raiz.title ("ventana")
+#raiz.resizable(0,0)
+#raiz.geometry("250x50+500+250")
+#raiz.button.place(X=60, y=40, width=100; height=30)
+# raiz  crea la ventana
+raiz.mainloop()
